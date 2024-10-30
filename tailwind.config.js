8e393960f82e9cb093945e4ceb639b1ed3172267
Kiretwo <eritor00991@stud.noroff.no>
2024-10-30 14:24:15 +0100

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        special: ['Playwrite GB S', 'sans-serif'],
+      },
+}, },
+  plugins: [],
+};
